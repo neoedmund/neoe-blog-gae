96166bfa5a13d8607e479488b9a668dec306a2ba
neoedmund <neoedmund@2055cfe4-a4e8-11dd-adfd-a1ec4d499d49>
2009-03-30 04:56:30 +0000

    eve

--- trunk/app/eveapirss.py
+++ trunk/app/eveapirss.py
@@ -12,14 +12,18 @@ from google.appengine.ext import db
 import httplib, urllib
 import logging
 from XmlUtil import getXMLTree
+from google.appengine.api.urlfetch import fetch
+from urllib import urlencode
+import simplejson as json
 
-implpaths=["/char/WalletJournal","/char/WalletTransactions","/char/SkillQueue","/char/IndustryJobs","/char/MarketOrders"]
+implpaths=[#"/char/WalletJournal",\
+		"/char/WalletTransactions","/char/SkillQueue","/char/IndustryJobs","/char/MarketOrders"]
 def html2post(xml,path,cid):
-	t=getXMLTree(xml)	
+	t=getXMLTree(xml)
 	posts=[]
 	cnt=0
 	if not hasattr(t.eveapi[0],"result"):
-		logging.info(xml[:200])
+		logging.debug(xml[:200])
 		return []
 	if path=="/char/IndustryJobs":
 		#mycat='IndustryJobs.%s'%cid
@@ -28,7 +32,7 @@ def html2post(xml,path,cid):
 		#oldvalues=[]
 		#for row in query.fetch():
 		#	oldkeys.append(row.xkey)
-		#	oldvalues.append(row.xvalue)			
+		#	oldvalues.append(row.xvalue)
 		for r in t.eveapi[0].result[0].rowset[0].row:
 			newvalue='jobID="%s" assemblyLineID="%s" containerID="%s" installedItemID="%s" installedItemLocationID="%s" installedItemQuantity="%s" installedItemProductivityLevel="%s" installedItemMaterialLevel="%s" installedItemLicensedProductionRunsRemaining="%s" outputLocationID="%s" installerID="%s" runs="%s" licensedProductionRuns="%s" installedInSolarSystemID="%s" containerLocationID="%s" materialMultiplier="%s" charMaterialMultiplier="%s" timeMultiplier="%s" charTimeMultiplier="%s" installedItemTypeID="%s" outputTypeID="%s" containerTypeID="%s" installedItemCopy="%s" completed="%s" completedSuccessfully="%s" installedItemFlag="%s" outputFlag="%s" activityID="%s" completedStatus="%s" installTime="%s" beginProductionTime="%s" endProductionTime="%s" pauseProductionTime="%s"'\
 			    %(r.jobID, r.assemblyLineID, r.containerID, r.installedItemID, r.installedItemLocationID, r.installedItemQuantity, r.installedItemProductivityLevel, r.installedItemMaterialLevel, r.installedItemLicensedProductionRunsRemaining, r.outputLocationID, r.installerID, r.runs, r.licensedProductionRuns, r.installedInSolarSystemID, r.containerLocationID, r.materialMultiplier, r.charMaterialMultiplier, r.timeMultiplier, r.charTimeMultiplier, r.installedItemTypeID, r.outputTypeID, r.containerTypeID, r.installedItemCopy, r.completed, r.completedSuccessfully, r.installedItemFlag, r.outputFlag, r.activityID, r.completedStatus, r.installTime, r.beginProductionTime, r.endProductionTime, r.pauseProductionTime)
@@ -44,9 +48,13 @@ def html2post(xml,path,cid):
 			posts.append(p)
 			cnt+=1
 			if cnt>20:break
-		
-		
+
+
 	elif path=="/char/MarketOrders":
+		typeids=[]
+		for r in t.eveapi[0].result[0].rowset[0].row:
+			typeids.append(int(r.typeID))
+		typeNameMap=getTypeNameMap(typeids)
 		for r in t.eveapi[0].result[0].rowset[0].row:
 			s  = 'orderID="%s" charID="%s" stationID="%s" volEntered="%s" volRemaining="%s" minVolume="%s" orderState="%s" typeID="%s" range="%s" accountKey="%s" duration="%s" escrow="%s" price="%s" bid="%s" issued="%s"'\
 			    %(r.orderID, r.charID, r.stationID, r.volEntered, r.volRemaining, r.minVolume, r.orderState, r.typeID, r.range, r.accountKey, r.duration, r.escrow, r.price, r.bid, r.issued)
@@ -56,14 +64,14 @@ def html2post(xml,path,cid):
 			p.text = s
 			p.pubDate = datetime.datetime(*(time.strptime(r.issued,"%Y-%m-%d %H:%M:%S")[0:6]))
 			p.author = "eve"
-			p.title ="MarketOrders: orderID=%s,typeID=%s,vol=%s/%s" \
-			    % (r.orderID, r.typeID, r.volRemaining, r.volEntered)
+			p.title ="MarketOrders: orderID=%s,typeID=%s(%s),vol=%s/%s" \
+			    % (r.orderID, r.typeID, typeNameMap[int(r.typeID)], r.volRemaining, r.volEntered)
 			if  int(r.duration<=1):
 				p.title ="[expiring]"+p.title
 			posts.append(p)
 			cnt+=1
 			if cnt>20:break
-	else:	                                                                                                        
+	else:
 		for r in t.eveapi[0].result[0].rowset[0].row:
 			if path=="/char/WalletJournal":
 				key="WalletJournal.%s.%s"%(cid, r.refID)
@@ -77,7 +85,7 @@ def html2post(xml,path,cid):
 				p.pubDate = datetime.datetime(*(time.strptime(r.date,"%Y-%m-%d %H:%M:%S")[0:6]))
 				p.author = "eve"
 				p.title = "WalletJournal: %s" %r.amount
-				posts.append(p)			
+				posts.append(p)
 			elif path=="/char/WalletTransactions":
 				key="WalletTransactions.%s.%s"%(cid, r.transactionID)
 				#if existed(key):continue
@@ -90,7 +98,7 @@ def html2post(xml,path,cid):
 				p.pubDate = datetime.datetime(*(time.strptime(r.transactionDateTime,"%Y-%m-%d %H:%M:%S")[0:6]))
 				p.author = "eve"
 				p.title = "WalletTransactions: %s" %"%s %s * %s at isk %s" % (r.transactionType, r.typeName, r.quantity, r.price)
-				posts.append(p)		    
+				posts.append(p)
 			elif path=="/char/SkillQueue":
 				key="SkillQueue.%s.%s.%s.%s"%(cid, r.typeID,r.level,r.startSP )
 				#if existed(key):continue
@@ -103,32 +111,47 @@ def html2post(xml,path,cid):
 				p.pubDate = datetime.datetime(*(time.strptime(r.startTime,"%Y-%m-%d %H:%M:%S")[0:6]))
 				p.author = "eve"
 				p.title ="SkillQueue: %s" % "%s lv %s (%s-%s)" % (r.typeID, r.level, r.startTime, r.endTime)
-				posts.append(p)				
+				posts.append(p)
 			else:
-				logging.info("unkown path:%s" % path)
+				logging.debug("unkown path:%s" % path)
 				break
 			cnt+=1
 			if cnt>20:break
-			
+
 	return posts
-	
+def getTypeNameMap(typeids):
+	m={}
+	if len(typeids)==0:return m
+	gql="SELECT * FROM eve_invTypes where typeID in (%s)"%(",".join(str(typeids)))	
+	logging.debug("query %s"%gql)
+	resp=fetch("http://neoe-table.appspot.com/a?i=%s"%urlencode(gql))
+	if resp.status_code==200:
+		o=json.loads(resp.content)
+		if o[0]=="0":
+			r=o[3]
+			p1=r.index('typeID')
+			p2=r.index('typeName')
+			for x in r[1:]:
+				m[int(x[p1])]=x[p2]
+	else: logging.debug("errorcode:%s"%(resp.status_code))
+	return m
 class XFeed(db.Model):
 	xkey = db.StringProperty()
 	xvalue = db.StringProperty()
 	xcat = db.StringProperty()
-	
+
 class RssFeed(db.Model):
 	data = db.StringProperty()
 
 def existed(key):
 	feed=RssFeed.gql("WHERE data = :1 ", key).get()
 	if feed:
-		logging.info("existed:%s" % key)
+		logging.debug("existed:%s" % key)
 		return True
 	else:
 		feed=RssFeed(data=key)
 		feed.put()
-		logging.info("new:%s" % key)
+		logging.debug("new:%s" % key)
 		return False
 
 def rss(cid, uid, k):
@@ -150,16 +173,16 @@ def getposts(cid, uid, k, path):
 	conn.request("POST", "%s.xml.aspx"%path, params, headers)
 	response = conn.getresponse()
 	#print response.status, response.reason
-	if response.status!=200: 
+	if response.status!=200:
 		posts=[]
-		logging.info("response.status(%s)"%response.status)
+		logging.debug("response.status(%s)"%response.status)
 	else:
 		data = response.read()
 		conn.close()
 		posts = html2post(data, path, cid)
-	return posts	
-	
-	
-	
+	return posts
+
+
+
 if __name__=="__main__":
 	print rss("","","")
